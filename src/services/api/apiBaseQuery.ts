bb7f1d38de10d6206c3eb957cd636ded95b018ea
BPierrick <pierrick.bellut95@gmail.com>
2021-11-10 16:14:01 +0100

    chore: add FHIR_API_AUTH_TOKEN in constants

--- src/services/api/apiBaseQuery.ts
+++ src/services/api/apiBaseQuery.ts
@@ -1,9 +1,14 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-import { API_URL } from "../../constants";
+import { API_URL, FHIR_API_AUTH_TOKEN } from "../../constants";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_URL,
+  prepareHeaders: (headers) => {
+    headers.set("Cache-Control", "no-cache");
+    FHIR_API_AUTH_TOKEN && headers.set("Authorization", FHIR_API_AUTH_TOKEN);
+    return headers;
+  },
 });
 
 export const apiBaseQuery: ReturnType<typeof fetchBaseQuery> = async (
