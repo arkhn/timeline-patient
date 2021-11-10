8a4565bb910300f93473c6ef9476bfe410002cf6
BPierrick <pierrick.bellut95@gmail.com>
2021-11-10 16:14:01 +0100

    chore: add FHIR_API_AUTH_TOKEN in constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,14 +2,21 @@ type EnhancedWindow = typeof window & {
   env: {
     PUBLIC_URL: string;
     API_URL?: string;
+    FHIR_API_AUTH_TOKEN?: string;
   };
 };
 
-let { PUBLIC_URL, REACT_APP_API_URL: API_URL } = process.env;
+let {
+  PUBLIC_URL,
+  REACT_APP_API_URL: API_URL,
+  REACT_APP_FHIR_API_AUTH_TOKEN: FHIR_API_AUTH_TOKEN,
+} = process.env;
 
 // when using the app with a production build, environment variables are templated in index.html.
 if (process.env.NODE_ENV === "production") {
-  ({ PUBLIC_URL, API_URL } = (window as EnhancedWindow).env);
+  ({ PUBLIC_URL, API_URL, FHIR_API_AUTH_TOKEN } = (
+    window as EnhancedWindow
+  ).env);
 }
 
-export { PUBLIC_URL, API_URL };
+export { PUBLIC_URL, API_URL, FHIR_API_AUTH_TOKEN };
