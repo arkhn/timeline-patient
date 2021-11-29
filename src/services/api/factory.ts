3fde6c4e055d382141ad980da15b93a3f3ea4332
BPierrick <pierrick.bellut95@gmail.com>
2021-11-29 16:16:45 +0100

    text: ResourceCard unit tests & factories

--- src/services/api/factory.ts
+++ src/services/api/factory.ts
@@ -1,4 +1,7 @@
 import {
+  ICarePlan,
+  ICondition,
+  IEncounter,
   IPatient,
   IResourceList,
   PatientGenderKind,
@@ -6,6 +9,7 @@ import {
 import * as faker from "faker";
 import { Factory } from "fishery";
 
+import { TERMINOLOGY_SYSTEM_URL } from "../../constants";
 import { Bundle } from "./api";
 
 export const bundleFactory = <ResourceType extends IResourceList>(): Factory<
@@ -22,7 +26,63 @@ export const bundleFactory = <ResourceType extends IResourceList>(): Factory<
 export const patientFactory = Factory.define<IPatient>(({ sequence }) => ({
   id: sequence.toString(),
   resourceType: "Patient",
-  birthDate: faker.date.past().toString(),
+  birthDate: faker.date.past().toISOString(),
   name: [{ given: [faker.name.firstName()], family: faker.name.lastName() }],
   gender: faker.name.gender() as PatientGenderKind,
 }));
+
+export const conditionFactory = Factory.define<ICondition>(
+  ({ sequence, associations }) => ({
+    id: sequence.toString(),
+    resourceType: "Condition",
+    code: associations.code || {
+      coding: [
+        {
+          system: faker.internet.url(),
+          code: faker.lorem.word(),
+          display: faker.lorem.words(4),
+        },
+      ],
+    },
+    meta: associations.meta || {
+      tag: [{ system: TERMINOLOGY_SYSTEM_URL, display: faker.lorem.word() }],
+    },
+    onsetDateTime:
+      associations.onsetDateTime || faker.date.past().toISOString(),
+    subject: associations.subject || {},
+  })
+);
+
+export const encounterFactory = Factory.define<IEncounter>(
+  ({ sequence, associations }) => {
+    const periodEnd =
+      associations.period?.end || faker.date.past().toISOString();
+    const periodStart =
+      associations.period?.start ||
+      faker.date.past(undefined, periodEnd).toISOString();
+    return {
+      id: sequence.toString(),
+      resourceType: "Encounter",
+      location: associations.location || [
+        { location: { display: faker.lorem.word() } },
+      ],
+      meta: associations.meta || {
+        tag: [{ system: TERMINOLOGY_SYSTEM_URL, display: faker.lorem.word() }],
+      },
+      period: {
+        ...associations.period,
+        start: periodStart,
+        end: periodEnd,
+      },
+      class: associations.subject || {},
+    };
+  }
+);
+
+export const carePlanFactory = Factory.define<ICarePlan>(
+  ({ sequence, associations }) => ({
+    id: sequence.toString(),
+    resourceType: "CarePlan",
+    subject: associations.subject || {},
+  })
+);
