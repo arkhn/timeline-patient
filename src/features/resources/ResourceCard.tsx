b0e4c52f281128df9bea6256dc9ed6740937ec18
BPierrick <pierrick.bellut95@gmail.com>
2022-01-03 10:58:28 +0100

    rollback: remove resource type translations

--- src/features/resources/ResourceCard.tsx
+++ src/features/resources/ResourceCard.tsx
@@ -2,7 +2,6 @@ import React, { useMemo } from "react";
 
 import type { IResourceList } from "@ahryman40k/ts-fhir-types/lib/R4";
 import { Card, CardHeader } from "@mui/material";
-import { useTranslation } from "react-i18next";
 
 import Tag from "common/components/Tag";
 import ConditionCard from "features/conditions/ConditionCard";
@@ -16,7 +15,6 @@ type ResourceCardProps = {
 };
 
 const ResourceCard = ({ resource }: ResourceCardProps): JSX.Element => {
-  const { t } = useTranslation();
   const card: JSX.Element = useMemo(() => {
     switch (resource.resourceType) {
       case "Condition":
@@ -30,13 +28,13 @@ const ResourceCard = ({ resource }: ResourceCardProps): JSX.Element => {
         return (
           <Card>
             <CardHeader
-              title={<Tag value={t(resource.resourceType)} color="#555" />}
+              title={<Tag value={resource.resourceType} color="#555" />}
             />
             <ResourceCardActions resource={resource} />
           </Card>
         );
     }
-  }, [resource, t]);
+  }, [resource]);
 
   return card;
 };
