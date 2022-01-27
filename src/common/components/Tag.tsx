51901207532fef344e112ab73512217870e0337c
BPierrick <pierrick.bellut95@gmail.com>
2022-01-27 10:57:43 +0100

    style: add size props for `Tag` component

--- src/common/components/Tag.tsx
+++ src/common/components/Tag.tsx
@@ -6,29 +6,34 @@ import { CSSProperties, DefaultTheme, makeStyles } from "@mui/styles";
 type TagProps = {
   value?: string;
   color?: CSSProperties["color"];
+  size?: "normal" | "small";
 };
 
-const useStyles = makeStyles<DefaultTheme, { color?: CSSProperties["color"] }>(
-  (theme) => ({
-    tagContainer: {
-      width: "fit-content",
-      paddingTop: theme.spacing(1),
-      paddingBottom: theme.spacing(0.8),
-      paddingInline: theme.spacing(1.5),
-      borderRadius: 50,
-      marginRight: theme.spacing(1),
-      backgroundColor: (props) => props.color,
-    },
-    tag: {
-      color: (props) =>
-        props.color && theme.palette.getContrastText(props.color),
-      fontSize: "0.9rem",
-    },
-  })
-);
+const useStyles = makeStyles<
+  DefaultTheme,
+  { color?: CSSProperties["color"]; size: "normal" | "small" }
+>((theme) => ({
+  tagContainer: {
+    width: "fit-content",
+    paddingTop: (props) =>
+      props.size === "small" ? theme.spacing(0.4) : theme.spacing(1),
+    paddingBottom: (props) =>
+      props.size === "small" ? theme.spacing(0.3) : theme.spacing(0.8),
+    paddingInline: (props) =>
+      props.size === "small" ? theme.spacing(0.8) : theme.spacing(1.5),
+    borderRadius: 50,
+    marginRight: (props) =>
+      props.size === "small" ? theme.spacing(0.6) : theme.spacing(1),
+    backgroundColor: (props) => props.color,
+  },
+  tag: {
+    color: (props) => props.color && theme.palette.getContrastText(props.color),
+    fontSize: (props) => (props.size === "small" ? "0.75rem" : "0.9rem"),
+  },
+}));
 
-const Tag = ({ value, color }: TagProps): JSX.Element => {
-  const classes = useStyles({ color });
+const Tag = ({ value, color, size = "normal" }: TagProps): JSX.Element => {
+  const classes = useStyles({ color, size });
   return (
     <div className={classes.tagContainer}>
       <Typography className={classes.tag}>{value}</Typography>
